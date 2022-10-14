5282cba2b5190b318c11873c20b5d22f168df141
Bryce Kellogg <bkellogg@tethers.com>
2022-10-13 19:14:47 -0700

    Fixed fish colors & added aliases

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -5,16 +5,17 @@ set fish_greeting
 
 if status is-interactive
 
-    # Fisher is a plugin manager for fish. Here
-    # we test plugins to to see if they are already
-    # installed, and if not we download & install them.
-    if not fisher list ilancosman/tide@v5 &> /dev/null
-        fisher install ilancosman/tide@v5
-    end
+    set -l DIRCOLORS_FILE "$HOME/.config/dircolors/solarized-dircolors.256dark"
+    set -q XDG_CONFIG_HOME && set -l DIRCOLORS_FILE "$XDG_CONFIG_HOME/dircolors/solarized-dircolors.256dark"
+    eval (dircolors -c $DIRCOLORS_FILE)
 
     # Aliases
     alias vim='nvim'
+    alias ll='exa -l --group-directories-first'
+    alias ls='exa --group-directories-first'
+    alias la='exa -la --group-directories-first'
 end
+
 
 # Config options for the tide prompt. These need to
 # be set outside the `is-interactive` block because
@@ -24,6 +25,6 @@ set -g tide_left_prompt_items pwd git character
 set -g tide_character_color green
 set -g tide_character_color_failure red
 set -g tide_git_color_branch yellow
-set -g tide_pwd_color_dirs green
-set -g tide_pwd_color_anchors green
-set -g tide_pwd_color_truncated_dirs green
+set -g tide_pwd_color_dirs 859900
+set -g tide_pwd_color_anchors 859900
+set -g tide_pwd_color_truncated_dirs 859900
