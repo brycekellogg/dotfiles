b398ef766f7e3e780aad313d9ffc1f943e245036
Bryce Kellogg <bryce@kellogg.org>
2023-06-13 20:29:35 -0700

    Neovim: fixed tree-sitter config to actually run

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,22 +1,30 @@
 --
 --
-return {{   
-    'nvim-treesitter/nvim-treesitter',      -- git URL of plugin
-    event = {"BufReadPost", "BufNewFile"},  -- lazy load plugin on these vim events
-    opts = {                                -- options to be passed to setup
-        ensure_installed = {"c", "cpp", "lua", "fish", "vim"},
-        ignore_installed = {"help"},
-        highlight = {
-            enable = true
-        },
+return {
+    {   
+        'nvim-treesitter/nvim-treesitter',      -- git URL of plugin
+        event = {"BufReadPost", "BufNewFile"},  -- lazy load plugin on these vim events
+        opts = {                                -- options to be passed to setup
+            ensure_installed = {"c", "cpp", "lua", "fish", "vim"},
+            ignore_installed = {"help"},
+            highlight = {
+                enable = true
+            },
 
-        -- Config for nvim-ts-rainbow
-        -- Currently disable until colors
-        -- are fixed. Looks bad right now.
-        rainbow = {
-            enable = false,
-            extended_mode = true,
-            max_file_lines = nil,
+            -- Config for nvim-ts-rainbow
+            -- Currently disable until colors
+            -- are fixed. Looks bad right now.
+            rainbow = {
+                enable = false,
+                extended_mode = true,
+                max_file_lines = nil,
+            },
         },
+        config = function(_, opts)
+            require('nvim-treesitter.configs').setup(opts)
+        end,
     },
-}}
+    {
+        'nvim-treesitter/playground',
+    },
+}
