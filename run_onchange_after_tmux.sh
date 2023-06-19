f33c0cedae5cf81570c8c7091c16241a08609876
Bryce Kellogg <bryce@kellogg.org>
2023-06-19 09:19:05 -0700

    Chezmoi: added tmux install script

---
+++ run_onchange_after_tmux.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# vim: ft=bash
+
+# Set bash to exit on the first error & print an error
+# message detailing which command produced the error.
+set -e
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "\"${last_command}\" command failed with exit code $?."' ERR
+
+
+# Section for installing on Fedora
+if [ "$CHEZMOI_OSID" == "linux-fedora" ]; then
+    sudo dnf install tmux  # Use distro version
+fi
+
+
+# Section for installing on macOS
+if [ "$CHEZMOI_OSID" == "darwin" ]; then
+    command -v brew    # Check required tools
+    brew install tmux  # use homebrew version
+fi
+
