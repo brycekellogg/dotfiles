2fe22ecbf9283a3241478f37d41ae39e59f88c20
Bryce Kellogg <bryce@kellogg.org>
2023-06-19 13:40:59 -0700

    Chezmoi: changed macos install for alacritty

--- run_onchange_after_alacritty.sh
+++ run_onchange_after_alacritty.sh
@@ -16,26 +16,8 @@ fi
 
 # Section for installing on macOS
 if [ "$CHEZMOI_OSID" == "darwin" ]; then
-
-    # Check required tools
-    command -v curl     # required to download
-    command -v hdiutil  # required to mount DMG
-    command -v mkdir    # required to create dest dir
-    command -v cp       # required to install files
-    command -v rm       # required to clean up
-
-    # Helper variables
-    ALACRITTY_URL="https://github.com/alacritty/alacritty/releases/download/v0.12.1/Alacritty-v0.12.1.dmg"
-    ALACRITTY_DMG="Alacritty.dmg"
-    ALACRITTY_VOLUME="/Volumes/Alacritty"
-    ALACRITTY_APP="$ALACRITTY_VOLUME/Alacritty.app"
-    ALACRITTY_DEST="$HOME/Applications"
-
-    # Download & install
-    curl -sLo $ALACRITTY_DMG $ALACRITTY_URL  # download DMG file
-    hdiutil attach $ALACRITTY_DMG            # mount disk image
-    mkdir -p $ALACRITTY_DEST                 # create user specific app directory
-    cp -R  $ALACRITTY_APP $ALACRITTY_DEST    # do the install (via cp)
-    hdiutil detach $ALACRITTY_VOLUME         # unmount disk image
-    rm -rf $ALACRITTY_DMG                    # clean up downloaded files
+    command -v brew     # required to install
+    command -v xattr    # required to remove developer warning
+    brew install alacritty  # use homebrew version
+    xattr -rd com.apple.quarantine /Applications/Alacritty.app/  # remove developer warning
 fi
