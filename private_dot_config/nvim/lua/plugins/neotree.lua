c3e5bfaccfc582c91090cb7bb7ff88751386a31a
Bryce Kellogg <bryce@kellogg.org>
2023-07-02 07:55:11 -0700

    Neovim: updated neotree config

--- private_dot_config/nvim/lua/plugins/neotree.lua
+++ private_dot_config/nvim/lua/plugins/neotree.lua
@@ -5,6 +5,14 @@ return {{
     opts = {
         add_blank_line_at_top = true,
         close_if_last_window = true,
+        default_component_configs = {
+            name = {
+                use_git_status_colors = false,
+            },
+        },
+        filesystem = {
+            use_libuv_file_watcher = true,
+        },
     },
     cmd = "Neotree",                -- lazy load plugin on this command
     keys = {                        -- lazy load plugin on these key presses
