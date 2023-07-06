caa04954823ca9305956563d00b51cf697bafaa5
Bryce Kellogg <bryce@kellogg.org>
2023-07-06 09:10:07 -0700

    Neovim: removed winbar from lualine

--- private_dot_config/nvim/lua/plugins/lualine.lua
+++ private_dot_config/nvim/lua/plugins/lualine.lua
@@ -31,38 +31,6 @@ return {{
             lualine_b = {},
             lualine_x = {},
         },
-        winbar = {
-            lualine_a = {},
-            lualine_b = {{
-                brycedebugb,
-                padding = 0,
-                color = 'LualineWinbarActive_b',
-            }},
-            lualine_c = {{
-                brycedebugc,
-                color = 'LualineWinbarActive_c',
-            }},
-            lualine_x = {{
-                'filename',
-                color = 'LualineWinbarActive_x',
-            }},
-            lualine_y = {},
-            lualine_z = {},
-        },
-        inactive_winbar = {
-            lualine_a = {},
-            lualine_b = {},
-            lualine_c = {{
-                'inactive',
-                color = 'LualineWinbarInactive_c',
-            }},
-            lualine_x = {{
-                'filename',
-                color = 'LualineWinbarInactive_x',
-            }},
-            lualine_y = {},
-            lualine_z = {}
-        },
     },
 }}
 
