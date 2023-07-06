4db86cccf8e0b72e95ee4ae0631f82832f4ba9e7
Bryce Kellogg <bryce@kellogg.org>
2023-07-06 09:11:57 -0700

    Neovim: removed more lualine winbar code

--- private_dot_config/nvim/lua/plugins/lualine.lua
+++ private_dot_config/nvim/lua/plugins/lualine.lua
@@ -1,18 +1,6 @@
 --
 --
 --
-
-local function brycedebugb()
-    local winid = vim.fn.win_getid()
-    local wininfo = vim.fn.getwininfo(winid)
-    local offset = wininfo[1]['textoff']
-    return string.rep('~', offset)
-end
-
-local function brycedebugc() return
-    [[                             c]]
-end
-
 return {{
     'nvim-lualine/lualine.nvim',
     event = 'VeryLazy',
@@ -21,9 +9,6 @@ return {{
             theme = 'solarized_dark',
             disabled_filetypes = {
                 'alpha',
-                winbar = {
-                    'neo-tree'
-                },
             },
             globalstatus = true,
         },
