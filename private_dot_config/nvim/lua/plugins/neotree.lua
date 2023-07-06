8e22196869f0b5b8eb9a7bb140871cb2f2984cc1
Bryce Kellogg <bryce@kellogg.org>
2023-07-05 20:01:03 -0700

    Neovim: added winbar to neo-tree

--- private_dot_config/nvim/lua/plugins/neotree.lua
+++ private_dot_config/nvim/lua/plugins/neotree.lua
@@ -1,7 +1,7 @@
 --
 --
 return {{   
-    'nvim-neo-tree/neo-tree.nvim',  -- git URL of plugin
+    'nvim-neo-tree/neo-tree.nvim',
     opts = {
         add_blank_line_at_top = true,
         close_if_last_window = true,
@@ -13,12 +13,15 @@ return {{
         filesystem = {
             use_libuv_file_watcher = true,
         },
+        source_selector = {
+            winbar = true,
+        },
     },
-    cmd = "Neotree",                -- lazy load plugin on this command
-    keys = {                        -- lazy load plugin on these key presses
+    cmd = "Neotree",
+    keys = {
         {
-            "<C-b>",                -- key combo
-            function()              -- function to execute
+            "<C-b>",
+            function()
                 require("neo-tree.command").execute({toggle = true})
             end,
             desc = "Toggle File Explorer"
