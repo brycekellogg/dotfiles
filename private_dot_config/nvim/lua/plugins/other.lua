fd025e3e80ca3fefc4d1177bee4aa0d28ba4ea72
Bryce Kellogg <bryce@kellogg.org>
2023-07-05 17:28:51 -0700

    Neovim: removed lualine from other.lua

--- private_dot_config/nvim/lua/plugins/other.lua
+++ private_dot_config/nvim/lua/plugins/other.lua
@@ -2,7 +2,6 @@ return {
     'alexghergh/nvim-tmux-navigation',
     'nvim-lua/plenary.nvim',
     'MunifTanjim/nui.nvim',
-    'nvim-lualine/lualine.nvim',
     'akinsho/bufferline.nvim',
     'lewis6991/gitsigns.nvim',
 }
