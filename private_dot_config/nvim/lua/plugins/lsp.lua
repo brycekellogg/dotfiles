657c8706e932b89796b1b02624b3eda12fdb8cce
Bryce Kellogg <bryce@kellogg.org>
2023-07-08 09:34:10 -0700

    Neovim: LSP config updates

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -4,22 +4,29 @@
 return {
     {
         'neovim/nvim-lspconfig',
-        config = function(_, opts)
-            require('lspconfig').lua_ls.setup({
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = {'vim'},
-                        },
-                        workspace = {
-                            library = vim.api.nvim_get_runtime_file("", true),
-                        },
-                        telemetry = {
-                            enable = false,
-                        },
+        opts = {
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = {'vim'},
+                    },
+                    workspace = {
+                        library = vim.api.nvim_get_runtime_file("", true),
+                    },
+                    telemetry = {
+                        enable = false,
                     },
                 },
+            },
+        },
+        config = function(_, opts)
+
+            vim.diagnostic.config({
+                virtual_text = true,
+                signs = false,
             })
+
+            require('lspconfig').lua_ls.setup(opts)
         end,
     },
     {
