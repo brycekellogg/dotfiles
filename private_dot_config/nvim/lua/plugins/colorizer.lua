fa70864b791214b8f0695e7ebabe2b41bda9ecf7
Bryce Kellogg <bryce@kellogg.org>
2023-07-09 08:34:26 -0700

    Neovim: added colorizer

---
+++ private_dot_config/nvim/lua/plugins/colorizer.lua
@@ -0,0 +1,8 @@
+--
+-- This plugin highlights color codes such as '#RRGGBB', etc
+-- https://github.com/norcalli/nvim-colorizer.lua
+return {{
+    'norcalli/nvim-colorizer.lua',
+    event = 'BufReadPost',
+    opts = {},
+}}
