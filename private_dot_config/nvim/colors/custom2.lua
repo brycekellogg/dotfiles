e6f3a18a46206f667d00e6896f2496cc44a0e4d8
Bryce Kellogg <bryce@kellogg.org>
2023-07-09 08:24:30 -0700

    Neovim: better pmenu highlight

--- private_dot_config/nvim/colors/custom2.lua
+++ private_dot_config/nvim/colors/custom2.lua
@@ -67,7 +67,6 @@ vim.api.nvim_set_hl(0, 'CursorLine',   {bg=bgLight})
 vim.api.nvim_set_hl(0, 'CursorLineNr', {fg=fgDefault, bg=bgLight})
 -- CursorLineFold
 -- CursorLineSign
---
 -- Directory
 vim.api.nvim_set_hl(0, 'DiffAdd',      {fg=green,  bg=bgLight})
 vim.api.nvim_set_hl(0, 'DiffChange',   {fg=yellow, bg=bgLight})
@@ -94,8 +93,8 @@ vim.api.nvim_set_hl(0, 'NormalFloat',  {fg=fgDefault, bg=bgLight})
 -- FloatBorder
 -- FloatTitle
 -- NormalNC
--- Pmenu
--- PmenuSel
+vim.api.nvim_set_hl(0, 'Pmenu',    {fg=fgDefault, bg=bgLight})
+vim.api.nvim_set_hl(0, 'PmenuSel', {fg=fgDefault, bg=bgLight10})
 -- PmenuKind
 -- PmenuKindSel
 -- PmenuExtra
