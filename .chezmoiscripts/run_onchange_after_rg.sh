ca8bba99cd106cf85a873a53a09c2e02d3ac7097
Bryce Kellogg <bryce@kellogg.org>
2023-07-15 14:44:15 -0700

    Chezmoi: added rg install script

---
+++ .chezmoiscripts/run_onchange_after_rg.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Set bash to exit on the first error & print an error
+# message detailing which command produced the error.
+set -e
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "\"${last_command}\" command failed with exit code $?."' ERR
+
+
+# Section for installing on Fedora
+if [ "$CHEZMOI_OSID" == "linux-fedora" ]; then
+    sudo dnf install ripgrep  # Use distro version
+fi
+
+
+# Section for installing on macOS
+if [ "$CHEZMOI_OSID" == "darwin" ]; then
+    command -v brew    # Check required tools
+    brew install ripgrep  # Use homebrew version
+fi
+
+command -v rg # make sure it installed correctly
+
+
+
