0ae2100ba8c81e1b3fe763cbc2ad8857c633f8fa
Bryce Kellogg <bryce@kellogg.org>
2023-07-15 14:12:55 -0700

    Neovim: don't hide dotfiles in vim tree

--- private_dot_config/nvim/lua/plugins/neotree.lua
+++ private_dot_config/nvim/lua/plugins/neotree.lua
@@ -38,6 +38,9 @@ return {{
         },
         filesystem = {
             use_libuv_file_watcher = true,
+            filtered_items = {
+                hide_dotfiles = false,
+            },
         },
         source_selector = {
             winbar = true,
