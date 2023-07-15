39b20676d6618b287b6ff7a9b42f5128681ac482
Bryce Kellogg <bryce@kellogg.org>
2023-07-15 07:03:03 -0700

    Neovim: added nvim-cmp

---
+++ private_dot_config/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,28 @@
+
+return {{
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'hrsh7th/cmp-cmdline',
+        'hrsh7th/cmp-vsnip',
+        'hrsh7th/vim-vsnip',
+    },
+    config = function()
+        require('cmp').setup({
+            snippet = {
+                expand = function(args)
+                    vim.fn["vsnip#anonymous"](args.body)
+                end,
+            },
+            sources = require('cmp').config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'vsnip' },
+                { name = 'buffer' },
+                })
+        })
+    end
+}}
+
+
