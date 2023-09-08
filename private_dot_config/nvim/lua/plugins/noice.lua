988a0b0430315e798269430f1c11e521ed975093
Bryce Kellogg <bryce@kellogg.org>
2023-09-08 09:44:34 -0700

    Neovim: fixed indentation

--- private_dot_config/nvim/lua/plugins/noice.lua
+++ private_dot_config/nvim/lua/plugins/noice.lua
@@ -2,38 +2,38 @@
 --
 --
 return {{
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {
-      routes = {
-        {
-            filter = {event = 'msg_show', kind = '', find = 'written'},
-            opts = {skip = true},
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+        routes = {
+            {
+                filter = {event = 'msg_show', kind = '', find = 'written'},
+                opts = {skip = true},
+            },
+            {
+                filter = {event = 'msg_show', kind = '', find = 'lines yanked'},
+                opts = {skip = true},
+            },
+            {
+                filter = {event = 'msg_show', kind = '', find = 'more lines'},
+                opts = {skip = true}
+            },
+            {
+                filter = {event = 'msg_show', kind = '', find = 'fewer lines'},
+                opts = {skip = true}
+            },
         },
-        {
-            filter = {event = 'msg_show', kind = '', find = 'lines yanked'},
-            opts = {skip = true},
-        },
-        {
-            filter = {event = 'msg_show', kind = '', find = 'more lines'},
-            opts = {skip = true}
-        },
-        {
-            filter = {event = 'msg_show', kind = '', find = 'fewer lines'},
-            opts = {skip = true}
-        },
-      },
 
-      lsp = {
-          -- This section controls showing function signatures. We disable the
-          -- auto_open so that you don't get function docs when typing a function.
-          signature = {
-              auto_open = {enabled = false},
-          },
-      },
-  },
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-    "rcarriga/nvim-notify",
+        lsp = {
+            -- This section controls showing function signatures. We disable the
+            -- auto_open so that you don't get function docs when typing a function.
+            signature = {
+                auto_open = {enabled = false},
+            },
+        },
+    },
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify",
     }
 }}
