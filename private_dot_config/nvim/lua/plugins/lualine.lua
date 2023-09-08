fbf1af505906410b09899a8b542c22666f7d058e
Bryce Kellogg <bryce@kellogg.org>
2023-09-08 11:32:38 -0700

    Neovim: lualine uses different event

--- private_dot_config/nvim/lua/plugins/lualine.lua
+++ private_dot_config/nvim/lua/plugins/lualine.lua
@@ -3,7 +3,7 @@
 --
 return {{
     'nvim-lualine/lualine.nvim',
-    event = 'BufAdd',
+    event = 'BufEnter',
     dependencies = {
         'nvim-tree/nvim-web-devicons',
         'SmiteshP/nvim-navic',
