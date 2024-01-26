f77297d2c5cc17257dc159a465ca8980d0994b1a
Bryce Kellogg <bryce.kellogg@leostella.com>
2024-01-26 09:26:10 -0800

    Chezmoi: build tmux from source

--- .chezmoiscripts/run_onchange_after_tmux.sh
+++ .chezmoiscripts/run_onchange_after_tmux.sh
@@ -15,7 +15,21 @@ fi
 
 # Section for installing on Ubuntu
 if [ "$DOTFILES_OSID" == "linux-ubuntu" ] || [ "$DOTFILES_OSID" == "linux-ubuntu-wsl" ]; then
-    sudo apt install tmux  # Use distro version
+
+    TMUX_BUILD_DIR=$(mktemp -d)
+    TMUX_DEST_DIR="$HOME/.local/"
+    TMUX_URL="https://github.com/tmux/tmux/releases/download/3.3a/tmux-3.3a.tar.gz"
+
+    # Required to build
+    sudo apt-get -y -qq install libevent-dev
+    
+    curl -sL "$TMUX_URL" | tar -xz -C "$TMUX_BUILD_DIR"
+    env -C "$TMUX_BUILD_DIR/tmux-3.3a/" ./configure --prefix="$TMUX_DEST_DIR"
+    make -C "$TMUX_BUILD_DIR/tmux-3.3a/"
+    make -C "$TMUX_BUILD_DIR/tmux-3.3a/" install
+    
+
+    rm -rf "$TMUX_BUILD_DIR"
 fi
 
 # Section for installing on macOS
@@ -24,4 +38,6 @@ if [ "$DOTFILES_OSID" == "darwin" ]; then
     brew install tmux  # use homebrew version
 fi
 
-command -v tmux # make it installed correctly
+# make it installed correctly
+export PATH=$PATH:"$HOME/.local/bin/"
+command -v tmux
