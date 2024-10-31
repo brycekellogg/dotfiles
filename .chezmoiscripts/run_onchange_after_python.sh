57e26d47ecfcf677cdeb2d7e5bc5959298cb13e7
Bryce Kellogg <bryce.kellogg@leostella.com>
2024-10-31 08:33:58 -0700

    Chezmoi: fix python package install for macos

--- .chezmoiscripts/run_onchange_after_python.sh
+++ .chezmoiscripts/run_onchange_after_python.sh
@@ -10,12 +10,14 @@ trap 'echo "\"${last_command}\" command failed with exit code $?."' ERR
 # Section for installing on Fedora
 if [ "$DOTFILES_OSID" == "linux-fedora" ]; then
     sudo dnf install -y python3 python3-pip python3-virtualenv bpython
+    pip3 install neovim
 fi
 
 
 # Section for installing on Fedora
 if [ "$DOTFILES_OSID" == "linux-ubuntu-wsl" ] || [ "$DOTFILES_OSID" == "linux-ubuntu" ]; then
     sudo apt-get install -y python3 python3-pip python3-venv bpython
+    pip3 install neovim
 fi
 
 
@@ -24,14 +26,11 @@ fi
 if [ "$DOTFILES_OSID" == "darwin" ]; then
     command -v brew    # Check required tools
     brew install python3 bpython
+    pip3 install --break-system-packages neovim
 fi
 
 # make sure everything installed correctly
 command -v python3
 command -v pip3
 command -v bpython
-
-# Use pip to install packages
-pip3 install neovim
-
 
